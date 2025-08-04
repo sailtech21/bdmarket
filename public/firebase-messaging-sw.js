072772317fbc2d7e085efa95b700adc3d3067f0e
sailtech21 <sailtech21@gmail.com>
2025-08-05 03:39:01 +0600

    connect

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 // // Initialize the Firebase app in the service worker by passing the generated config
 
 const firebaseConfig = {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    measurementId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyC65v-Tomb_C9S0HY0X8aSkUKVzE5olYyI",
+    authDomain: "bdmarket-726ff.firebaseapp.com",
+    projectId: "bdmarket-726ff",
+    storageBucket: "bdmarket-726ff.firebasestorage.app",
+    messagingSenderId: "432405771305",
+    appId: "1:432405771305:web:f935930aad4a70debe5838",
+    measurementId: "G-YXCLB4VZ40"
 };
 
 firebase?.initializeApp(firebaseConfig)
