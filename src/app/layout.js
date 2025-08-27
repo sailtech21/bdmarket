41be1a511720ff23ce99b8bf9ad55f7d957387c1
BDmarket2024 <bdmarket.xyz@gmail.com>
2025-08-27 18:58:28 +0600

    update
    update logo and adsense

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,8 +36,8 @@ export default async function RootLayout({ children }) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossOrigin="anonymous" />
       </head>
       <body>
-        {/* <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-xxxxxxxxxxxx"
-          crossOrigin="anonymous" strategy="afterInteractive" /> */}
+        {<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5463846316468391"
+     crossorigin="anonymous"></script>}
         <Script src="https://js.paystack.co/v1/inline.js" strategy="beforeInteractive" />
         <Providers >
           <Toaster position="top-center" reverseOrder={false} />
